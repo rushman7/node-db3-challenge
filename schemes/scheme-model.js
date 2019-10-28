d3ace360489e1622a219ff092cb90c84398d2c41
BuildTools <arthurpisakhov1@gmail.com>
2019-10-27 22:13:06 -0400

    MVP

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -18,14 +18,20 @@ function findById(id) {
 }
 
 function findSteps(id) {
-
+  return db('schemes')
+    .where({ id })
+    .select('schemes.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')    
+    .join('steps', 'schemes.id','=', 'steps.schemes.id')
 }
 
 function add(scheme) {
+  return db('schemes').insert(scheme)
 }
 
 function update(changes, id) {
+  return db('schemes').where({ id }).update(changes, id)
 }
 
 function remove(id) {
+  return db('schemes').where({ id }).del()
 }
